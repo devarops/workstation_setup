3ab44f9a2936ad2a9f78501dd47cd99b4e5bc146
Mario Villasante • GECI <42744723+mvillasante@users.noreply.github.com>
2023-10-03 10:25:57 -0600

    🚧 Update terraform config for Azure

--- src/provider.tf
+++ src/provider.tf
@@ -1,35 +1,12 @@
 terraform {
   required_providers {
-    digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
     }
   }
 }
 
-variable "do_token" {}
-variable "pvt_key" {}
-
-provider "digitalocean" {
-  token = var.do_token
-}
-
-data "digitalocean_ssh_key" "evaro-desktop" {
-  name = "evaro@saltapared"
-}
-
-data "digitalocean_ssh_key" "evaro-laptop" {
-  name = "evaro@tijereta"
-}
-
-data "digitalocean_ssh_key" "nepo-desktop" {
-  name = "nepo@pelicano"
-}
-
-data "digitalocean_ssh_key" "nepo-laptop" {
-  name = "nepo@petrel"
-}
-
-data "digitalocean_ssh_key" "provisioner" {
-  name = "provisioner"
+provider "azurerm" {
+  features {}
 }
