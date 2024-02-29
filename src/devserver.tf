6a950419f6d092ab3bc0411c5613440413edf413
Mario Villasante • GECI <42744723+mvillasante@users.noreply.github.com>
2024-02-29 13:00:09 -0600

    ➕😋 Expand os_disk size

--- src/devserver.tf
+++ src/devserver.tf
@@ -56,6 +56,7 @@ resource "azurerm_linux_virtual_machine" "devserver" {
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
+    disk_size_gb         = 128
   }
 
   source_image_reference {
