889de853e6f264751bcef943dbe15104abb4654b
Guillermo Olvera • GECI <guillermo.olvera@islas.org.mx>
2024-05-03 09:29:44 -0600

    🦡 Upgrade ubuntu image to 24.04

--- src/devserver.tf
+++ src/devserver.tf
@@ -61,8 +61,8 @@ resource "azurerm_linux_virtual_machine" "devserver" {
 
   source_image_reference {
     publisher = "canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
     version   = "latest"
   }
 }
